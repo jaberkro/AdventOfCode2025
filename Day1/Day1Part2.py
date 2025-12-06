ceef84b691cc76df5bed9446d4b08e4b0bda7f53
jaberkro <jaberkro@student.codam.nl>
2025-12-06 17:32:59 +0100

    Part 2 working

---
+++ Day1/Day1Part2.py
@@ -0,0 +1,24 @@
+previousDialPointer = 50
+dialPointer = 50
+amountsPointingAt0AfterRotation = 0
+amountsPointingAt0DuringRotation = 0
+
+with open('Input.txt', 'r') as file:
+    for line in file:
+        dialPointer = dialPointer + int(line)
+        if dialPointer % 100 == 0:
+            amountsPointingAt0AfterRotation += 1
+        while dialPointer < 0:
+            if previousDialPointer != 0:
+                amountsPointingAt0DuringRotation += 1
+            dialPointer += 100
+            previousDialPointer = dialPointer
+        while dialPointer > 99:
+            if dialPointer != 100:
+                amountsPointingAt0DuringRotation += 1
+            dialPointer -= 100
+        previousDialPointer = dialPointer
+
+print("Password:", amountsPointingAt0AfterRotation, \
+        "+", amountsPointingAt0DuringRotation, \
+        "=", amountsPointingAt0AfterRotation + amountsPointingAt0DuringRotation)
