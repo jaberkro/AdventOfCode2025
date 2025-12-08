80dfc7f45e8968a285db565c5e3e389227f3c905
jaberkro <jaberkro@student.codam.nl>
2025-12-08 20:13:28 +0100

    both parts working

---
+++ Day7/Day7Part1.py
@@ -0,0 +1,18 @@
+totalSplitted = 0
+beams = []
+
+with open('Input.txt', 'r') as file:
+    for line in file:
+        if 'S' in line:
+            beams.append(line.index('S'))
+        else:
+            for index in range(0, len(line) - 1):
+                if line[index] == '^' and index in beams:
+                    while index in beams:
+                        beams.remove(index)
+                    beams.append(index - 1)
+                    beams.append(index + 1)
+                    totalSplitted += 1
+                    
+print("total times splitted:", totalSplitted)
+
---
+++ Day7/Day7Part2.py
@@ -0,0 +1,36 @@
+totalTimelines = 1
+beams = {}  
+
+def splitBeams(index):
+    newTimelines = 0
+
+    try:
+        newTimelines = beams[index]
+    except:
+        return 0
+    try:
+        beams[index - 1] += newTimelines
+    except:
+        beams[index - 1] = newTimelines
+    try:
+        beams[index + 1] += newTimelines
+    except:
+        beams[index + 1] = newTimelines
+    beams.pop(index)
+    return newTimelines
+
+with open('Input.txt', 'r') as file:
+    for line in file:
+        if 'S' in line:
+            beams[line.index('S')] = 1
+        elif '^' in line:
+            for index in range(0, len(line) - 1):
+                if line[index] == '^':
+                    try:
+                        while beams[index] > 0:
+                            totalTimelines += splitBeams(index)
+                    except:
+                        continue
+                    
+print("total timelines:", totalTimelines)
+
