8b505ced1c30d25cc14d8db95d9890e63c395595
jaberkro <jaberkro@student.codam.nl>
2026-01-02 12:02:12 +0100

    working

---
+++ Day11/Day11Part1.py
@@ -0,0 +1,17 @@
+devices = {}
+totalPaths = 0
+
+def findPaths(fromLocation):
+    pathsFound = 0
+    if fromLocation == 'out':
+        return 1
+    for locations in devices[fromLocation]:
+        pathsFound += findPaths(locations)
+    return pathsFound
+
+with open('Input.txt', 'r') as file:
+    for line in file:        
+        devices[line.split(': ')[0]] = ((line.split(': ')[1]).split('\n')[0]).split(' ')
+
+totalPaths = findPaths('you')
+print("Total different paths:", totalPaths)
